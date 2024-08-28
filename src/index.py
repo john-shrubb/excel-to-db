e79101147f458705d8d1212218ff75a73b3b8596
John Shrubb <john-shrubb@users.noreply.github.com>
2024-08-28 20:32:50 +0100

    Add some support for command line arguments.

--- src/index.py
+++ src/index.py
@@ -1,13 +1,24 @@
 import openpyxl
+import argparse
 from custom_types.data_type_enum import DataTypeEnum
 from custom_types.data_type import DataType
 from helper_functions.sql_identifier_check import ident_check
 
+# Argument parsing stuff.
+parser = argparse.ArgumentParser(prog='Excel to DB', description='Convert an Excel file to a Database.')
+parser.add_argument('--file_path', type=str, help='The path to the Excel file to convert.')
+
 print('Excel -> DB v0.0.1')
 print('-------------------------')
 # Path will go from the current working directory when the script is executed.
-# file_path = input('Enter path to XLSX file: ')
-file_path = './exampleData/Example-1.xlsx'
+file_path = ''
+
+# Try to grab the file path from the arguments.
+if not parser.parse_args().file_path:
+	file_path = input('Enter the path to the XLSX file to convert: ')
+else:
+	file_path = parser.parse_args().file_path
+
 # Load the workbook
 wb = 0
 
@@ -41,17 +52,30 @@ for i in range(1, max_columns + 1):
 	print(f'Column {i}: {sheet_object.cell(row=1, column=i).value}')
 	while True:
 		db_column_name = input('Enter the name of this column in the database: ').strip()
+		if not ident_check(db_column_name):
+			print('Invalid column name.')
+			continue
+
+		# Prevent duplicate column names.
 		for data_type in data_types:
 			if data_type.db_column_name == db_column_name:
 				print('Column already exists.')
 				continue
-		user_input = input('Enter data type: ').strip().lower()
+		
+		# Grab the data type from user.
+		data_type_name = input('Enter data type: ').strip().lower()
 
 		# Display a help message.
-		if user_input == '/?':
+		if data_type_name == '/?':
 			print('Valid data types:')
 			for data_type in DataTypeEnum:
 				print(data_type.value)
+			print('If you are entering a custom data type (For example an enumerator) enter the name of the type.')
+			continue
+
+		# Check that the data type is a valid type of identifier.
+		if not ident_check(data_type_name):
+			print('Invalid data type name.')
 			continue
 
 		# If the data type is valid, add it to the data_types dictionary.
@@ -59,7 +83,7 @@ for i in range(1, max_columns + 1):
 			data_type = DataType(
 				table_column_name = str(sheet_object.cell(row=1, column=i).value),
 				db_column_name = db_column_name,
-				data_type = DataTypeEnum[user_input]
+				data_type = DataTypeEnum[data_type_name]
 			)
 			data_types.append(data_type)
 			break
