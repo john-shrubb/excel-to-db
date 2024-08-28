3f4c8ddd186337830458048cfde3093ac6fc5c51
John Shrubb <john-shrubb@users.noreply.github.com>
2024-08-28 20:05:39 +0100

    Fix issue with stringify helper function

--- src/helper_functions/stringify_value.py
+++ src/helper_functions/stringify_value.py
@@ -2,7 +2,7 @@ def stringify_value(value: str) -> str:
 	"""
 	Convert a value into a string format which can be passed into an SQL query.
 	"""
-	nvalue = value.split('')
+	nvalue = list(value)
 	nvalue.append('\'')
 	nvalue.insert(0, '\'')
 	return ''.join(nvalue)
