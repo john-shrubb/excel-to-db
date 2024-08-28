5dfa884ab2af7a43182592e8e7df19385cd768ca
John Shrubb <john-shrubb@users.noreply.github.com>
2024-08-28 20:31:38 +0100

    Minor fix to SQL ident check

--- src/helper_functions/sql_identifier_check.py
+++ src/helper_functions/sql_identifier_check.py
@@ -5,6 +5,10 @@ def ident_check(value : str) -> bool:
 	"""
 	Checks if a string is a valid SQL identifier.
 	"""
+	# Prevent an out of range error.
+	if len(value) == 0:
+		return False
+
 	# Check that the identifier begins with a letter
 	if not value[0].isalpha() and not value[0] == '_':
 		return False
@@ -18,9 +22,3 @@ def ident_check(value : str) -> bool:
 		):
 			return False
 	return True
-# Test cases
-print(ident_check("valid_identifier"))  # True
-print(ident_check("_underscore"))  # True
-print(ident_check("123"))  # False
-print(ident_check("$dollar$"))  # True
-print(ident_check("invalid-identifier"))  # False
