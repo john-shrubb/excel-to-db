e0abb4407876fb49f21c180cde198c28347c6385
John Shrubb <john-shrubb@users.noreply.github.com>
2024-09-01 13:28:04 +0100

    Move argument parser to args.py

--- src/index.py
+++ src/index.py
@@ -10,20 +10,7 @@ from getpass import getpass
 from helper_functions.sql_identifier_check import ident_check
 import signal
 import json
-
-parser = ArgumentParser(description='Excel -> DB: Read an Excel file and insert the data into a PostgreSQL table.')
-parser.add_argument('--env-path', '-e', type=str, dest='envpath', help='The path to the .env file to load. Defaults to .env.')
-parser.add_argument('--table-name', '-t', type=str, dest='tablename', help='The name of the table to insert the data into.')
-parser.add_argument('--json-path', '-j', type=str, dest='jsonpath', help='The path of the JSON file with column data to load.')
-parser.add_argument('--assume-yes', '-y', action='store_true', dest='assumeyes', help='Assume yes to prompts.')
-
-group = parser.add_argument_group('XLSX Settings')
-group.add_argument('--file-path', '-f', type=str, dest='filepath', help='The path to the Excel file to load.')
-group.add_argument('--sheet-name', '-s', type=str, dest='sheetname', help='The name of the sheet to load, if the XLSX file has multiple sheets.')
-
-group = parser.add_argument_group('Random ID Column Generation')
-group.add_argument('--rand-col-name', '-c', type=str, dest='randcolname', help='The name of the column to generate ID values for.')
-group.add_argument('--rand-col-length', '-l', type=int, dest='randcollength', help='The length of the random IDs to generate.')
+from args import parser
 
 args = parser.parse_args()
 
